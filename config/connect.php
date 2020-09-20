e334187d577a826a8efd23027e261b7edfe435a4
Daniel Ruiz <danielruiz@iMac.local>
2020-09-20 23:08:32 +0200

    Change database error connect message

--- config/connect.php
+++ config/connect.php
@@ -5,7 +5,7 @@
         $conn = new PDO($host, $user, $pass);
         //echo "Conectado";
     } catch (PDOException $e){
-        echo "¡Error! No se pudo conectado a la base de datos. " . $e;
+        echo "¡Error! No se pudo conectar a la base de datos. " . $e;
         die();
     }
 
